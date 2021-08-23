71627a51f12a3e5946c9232a68621a1f4d439d42
shafiqXikmal <mohdshafiqikmalmuh@gmail.com>
2021-08-23 18:07:52 +0800

    Added pseudocode

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hai bruh");
+// Working with APIs
+// We need to answer the following questions
+// - what is the endpoint (URL)?
+// - what query string/parameter do we need to provide?
+// - what are the required parameters?
+// -what optional parameters do we want to add?
